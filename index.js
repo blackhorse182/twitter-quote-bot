c76d9ed3ea4d32f4d058d80c5a45a7a86e9bb6e3
blackhorse182 <william.dumas182@gmail.com>
2025-04-02 19:18:30 +0200

    Remove Express server setup from index.js

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const { TwitterApi } = require('twitter-api-v2');
 const schedule = require('node-schedule');
 const axios = require('axios');
-const express = require('express');
 require('dotenv').config();
 
-const app = express();
-const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
-  console.log(`Bot démarré sur le port ${PORT}! Posts toutes les 6 heures.`);
-});
+
+
 const client = new TwitterApi({
     appKey: 'MQnQJz6JfQ6FdbhGjmsgtN5aE', // Remplace par ta vraie API Key
     appSecret: 'MO4J0paVS0kvmPvSh715OjDU5R8cjJuWlOPN6Zz5JDPZqgmb5G',         // Remplace par ton vrai API Secret
